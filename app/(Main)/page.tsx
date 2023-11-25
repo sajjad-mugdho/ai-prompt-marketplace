7d4901309ef5b6d0a55f0365d4cb18dfcdafbab5
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-11-26 02:22:58 +0600

    [Done] Navbar

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
       <Header activeItem={0}></Header>
+      <Hero />
     </div>
   );
 };
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import React from "react";
 import { useState } from "react";
 import Navigation from "../Navigation/Navigation";
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { FaBars } from "react-icons/fa";
 
 type Props = {
   activeItem: number;
@@ -10,6 +13,7 @@ type Props = {
 
 export const Header = ({ activeItem }: Props) => {
   const [active, setactive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -20,6 +24,14 @@ export const Header = ({ activeItem }: Props) => {
       }
     });
   }
+
+  const handleClose = (e: React.MouseEvent) => {
+    const target = e.target as HTMLElement;
+
+    if (target.id === "screen") {
+      setOpen(!open);
+    }
+  };
   return (
     <div
       className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32] transition-opacity ${
@@ -37,6 +49,48 @@ export const Header = ({ activeItem }: Props) => {
         <div className="flex">
           <Navigation activeItem={activeItem}></Navigation>
         </div>
+        <div className="flex items-center ml-10">
+          <AiOutlineSearch className="text-[25px] mr-5 cursor-pointer" />
+          {/* {to do autho} */}
+
+          <Link href={"/sign-in"}>
+            <CgProfile className="text-[25px]  cursor-pointer" />
+          </Link>
+        </div>
+      </div>
+      {/* To Do */}
+
+      {/* For Mobile Screen */}
+
+      <div className="w-full md:hidden flex items-center justify-between ">
+        <div>
+          <Link href={"/"}>
+            <h1 className="font-Inter text-3xl cursor-pointer">
+              Prompt <span className="text-[#64ff4c]">Market</span>
+            </h1>
+          </Link>
+        </div>
+        <div>
+          <FaBars
+            className="text-2xl cursor-pointer"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+
+        {open && (
+          <div
+            className="fixed md:hidden w-full h-screen top-0 left-0 z-[99999] bg-[unset]"
+            onClick={handleClose}
+            id="screen"
+          >
+            <div className="fixed bg-black h-screen top-0 right-0 w-[60%] z-[99999]">
+              <div className="mt-20 p-5">
+                <Navigation activeItem={activeItem} />
+              </div>
+            </div>
+          </div>
+          //   todos
+        )}
       </div>
     </div>
   );
