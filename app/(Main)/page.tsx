6672d3e2b859fb37877c3f845639cfbdf5cfe3b0
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-11-26 18:52:59 +0600

    [Done] About component

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,12 +1,42 @@
+"use client";
 import { Header } from "@/components/Header/Header";
-import Hero from "@/components/Hero/Hero";
+import About from "@/components/Route/About";
+import Hero from "@/components/Route/Hero";
+import Image from "next/image";
+
 import React from "react";
 
+import { useState, useEffect } from "react";
+
 const Page = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+    }
+  }, [isMounted]);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <div>
-      <Header activeItem={0}></Header>
-      <Hero />
+      <div className="banner">
+        <Header activeItem={0}></Header>
+        <Hero />
+      </div>
+      {/* <Image
+        src={"https://pixner.net/aikeu/assets/images/footer/shape-two.png"}
+        width={120}
+        height={120}
+        alt=""
+        className="absolute right-[-30px]"
+      /> */}
+      <br />
+      <div className="w-[95%] md:w-[90%] xl:w-[80%] 2xl:w-[75%] m-auto">
+        <About />
+      </div>
     </div>
   );
 };
