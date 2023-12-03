27fd396f06e967e84aaf56ef72e355ed0b384293
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-12-03 18:34:15 +0600

    [Done] Upload prompt section created

--- app/(Shop)/shop/create-prompt/page.tsx
+++ app/(Shop)/shop/create-prompt/page.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Page = (props: Props) => {
   return (
     <div className="flex w-full">
-      <div className="h-screen p-2 bg-[#111c42] md:w-[20%] xl:w-[17%]">
+      <div className="h-screen p-2 sticky top-0 left-0 z-20 bg-[#111c42] md:w-[20%] xl:w-[17%]">
         <ShopSidebar active={1} />
       </div>
       <div className="md:w-[80%] 2xl:w-[83%]">
--- components/Prompts/UploadPrompt.tsx
+++ components/Prompts/UploadPrompt.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { styles } from "@/utils/style";
 import { useAuth } from "@clerk/nextjs";
 import {
